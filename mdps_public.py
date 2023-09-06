d22dcd1f3044567bdbce63afcb2fe5a74a5f8550
RaviRevathy <85576629+RaviRevathy@users.noreply.github.com>
2023-09-06 10:23:15 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -122,9 +122,9 @@ if (selected == 'Heart Disease Prediction'):
     if st.button('Heart Disease Test Result'):
         #heart_prediction = heart_disease_model.predict([[age, sex, cp, trestbps, chol, fbs, restecg,thalach,exang,oldpeak,slope,ca,thal]])                          
         st.write(sex, cp)
-        if (heart_prediction[0] == 1):
-          heart_diagnosis = 'The person is having heart disease'
-        else:
-          heart_diagnosis = 'The person does not have any heart disease'
+    #     if (heart_prediction[0] == 1):
+    #       heart_diagnosis = 'The person is having heart disease'
+    #     else:
+    #       heart_diagnosis = 'The person does not have any heart disease'
 
     st.success(heart_diagnosis)
